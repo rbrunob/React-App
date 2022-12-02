3df3dc2a523782a57b8e6a523fe6dd2c4c0f9585
rbrunob <bruno.bragaramos@gmail.com>
2022-12-02 13:32:06 -0300

    update -- styles

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.scss";
+
+class Button extends React.Component {
+  render() {
+    return (
+      <button className="botao">
+        Botão
+      </button>
+    )
+  }
+}
+
+export default Button;
---
+++ src/pages/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Formulario from "../components/Formulario";
+import List from "../components/Lista";
+import "./style.scss";
+
+function App() {
+  return (
+    <div className="AppStyle">
+      <Formulario />
+      <List />
+    </div>
+  );
+}
+
+export default App;
