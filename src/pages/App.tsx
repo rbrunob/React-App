aea62d49a6e28c1f5f08b4a937c958ff47b078cc
rbrunob <bruno.bragaramos@gmail.com>
2022-12-02 13:47:48 -0300

    update --modules

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Formulario from "../components/Formulario";
 import List from "../components/Lista";
-import "./style.scss";
+import style from "./App.module.scss";
 
 function App() {
   return (
-    <div className="AppStyle">
+    <div className={style.AppStyle}>
       <Formulario />
       <List />
     </div>
