bc60256d48ade040117a766907336ed32f780691
rbrunob <bruno.bragaramos@gmail.com>
2022-12-05 13:46:25 -0300

    update adicionando itens

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,35 +1,22 @@
-import React from 'react';
+import Reac, { useState } from 'react'
+import { ITarefa } from '../../types/tarefa';
 import Item from './Item';
 import style from "./List.module.scss";
 
 
-function List() {
-    const tarefas = [
-        {
-            tarefa: 'React',
-            tempo: '02:00:00'
-        },
 
-        {
-            tarefa: 'JavaScript',
-            tempo: '01:00:00'
-        },
+function List({ tarefas }: { tarefas: ITarefa[] }) {
 
-        {
-            tarefa: 'TypeScript',
-            tempo: '03:00:00'
-        }
-    ];
     return (
         <aside className={style.listaTarefas}>
-            <h2> Estudos do dia </h2>
+            <h2>Estudos do dia</h2>
             <ul>
                 {tarefas.map((item, index) => (
-                   <Item 
-                    key={index}
-                    tarefa={item.tarefa}
-                    tempo={item.tempo}
-                   />
+                    <Item
+                        key={index}
+                        tarefa={item.tarefa}
+                        tempo={item.tempo}
+                    />
                 ))}
             </ul>
         </aside>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Cronometro from "../components/Cronometro";
 import Formulario from "../components/Formulario";
 import List from "../components/Lista";
+import { ITarefa } from "../types/tarefa";
 import style from "./App.module.scss";
 
 function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[] | []>([])
   return (
     <div className={style.AppStyle}>
-      <Formulario />
-      <List />
+      <Formulario setTarefas={setTarefas} />
+
+      <List
+        tarefas={tarefas}
+      />
+
       <Cronometro />
     </div>
   );
