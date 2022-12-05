21d3d5086c7569e7fd7c62ae90865c872e7c2635
rbrunob <bruno.bragaramos@gmail.com>
2022-12-05 10:21:59 -0300

    update styles -- cronometro

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Item from './Item';
 import style from "./List.module.scss";
 
 
@@ -24,14 +25,11 @@ function List() {
             <h2> Estudos do dia </h2>
             <ul>
                 {tarefas.map((item, index) => (
-                    <li key={index} className={style.item}>
-                        <h3>
-                            {item.tarefa}
-                        </h3>
-                        <span>
-                            {item.tempo}
-                        </span>
-                    </li>
+                   <Item 
+                    key={index}
+                    tarefa={item.tarefa}
+                    tempo={item.tempo}
+                   />
                 ))}
             </ul>
         </aside>
